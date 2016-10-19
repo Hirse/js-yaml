7b517903a6dfae81c39126773a80ea7595145216
Hirse <jan.pilzer@gmx.de>
2016-10-19 20:00:33 +0200

    AST for merge and alias

---
+++ test/samples-common/construct-alias.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  alias: {
+    value: 1,
+    sequence: [ 1 ],
+    map: {
+      key: 1
+    }
+  },
+  anchor: {
+    value: 1,
+    sequence: [ 1 ],
+    map: {
+      key: 1
+    }
+  }
+};
